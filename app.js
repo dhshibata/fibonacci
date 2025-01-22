46e55c47b7f5bc7978a9ace1863bed2be236b3ad
dhshibata <dhshibata@ymail.ne.jp>
2025-01-22 20:37:29 +0900

    3種類のフィナボッチ数列の実装

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+// 連想配列使用(再帰)
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
@@ -14,4 +15,30 @@ function fib(n) {
 const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(fib(i));
+}
+
+// 配列使用(再帰)
+let memo2 = [0, 1];
+function fib2(n) {
+    if (memo2[n] !== undefined) {
+        return memo2[n];
+    }
+    memo2[n] = fib2(n - 1) + fib2(n - 2);
+    return memo2[n];
+}
+
+for (let i = 0; i <= length; i++) {
+    console.log(fib2(i));
+}
+
+// 配列使用(再帰なし)
+let memo3 = Array(41).fill(undefined);
+memo3[0] = 0;
+memo3[1] = 1;
+
+for (let i = 0; i <= length; i++) {
+    if(i >= 2) {
+        memo3[i] = memo3[i - 1] + memo3[i - 2];
+    }
+    console.log(memo3[i]);
 }
